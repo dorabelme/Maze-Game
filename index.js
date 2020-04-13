38e3bc3cf8dbcdf7947c49455250f38470cabcc3
Dora Belme <contact.dorabelme@gmail.com>
2020-04-12 23:18:10 -0700

    maze generation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 const engine = Engine.create();
@@ -14,33 +14,31 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-    world,
-    MouseConstraint.create(engine, {
-        mouse: Mouse.create(render.canvas)
-    })
-);
 
 // Wall
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 
 World.add(world, walls);
 
-// Random Shapes
-for (let i = 0;i < 50;i++) {
-    if (Math.random() > 0.5) {
-        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-    } else {
-        World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-            render: {
-                fillStyle: 'red'
-            }
-        }));
-    }
-}
+// Maze Generation
+
+const grid = Array(3).fill(null).map(() => Array(3).fill(false));
+const horizontals = Array(3).fill(null).map(() => Array(2).fill(false));
+const verticals = Array(3).fill(null).map(() => Array(2).fill(false));
+
+
+
+// for (let i = 0;i < 3;i++) {
+//     grid.push([]);
+//     for (let j = 0;j < 3;j++) {
+//         grid[i].push(false);
+//     }
+// }
+
+console.log(horizontals)
 
