8ecfff592ba31018e74862bf995eed76b42c3627
Dora Belme <contact.dorabelme@gmail.com>
2020-04-16 15:44:51 -0400

    walls, goal, ball added

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 
-const cells = 3;
+const cells = 10;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
     element: document.body, engine: engine, options: {
-        wireframes: false,
+        wireframes: true,
         width, height
     }
 });
@@ -18,10 +21,10 @@ Runner.run(Runner.create(), engine);
 
 // Wall
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
 
 World.add(world, walls);
@@ -113,3 +116,54 @@ const stepThroughCell = (row, column) => {
 };
 
 stepThroughCell(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(columnIndex * unitLength + unitLength / 2, rowIndex * unitLength + unitLength, unitLength, 10, { isStatic: true });
+        World.add(world, wall);
+    });
+});
+
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(columnIndex * unitLength + unitLength, rowIndex * unitLength + unitLength / 2, 10, unitLength, { isStatic: true });
+        World.add(world, wall);
+    })
+});
+
+// GOAL
+const goal = Bodies.rectangle(width - unitLength / 2, height - unitLength / 2, unitLength * .7, unitLength * .7, { isStatic: true });
+World.add(world, goal);
+
+// BALL
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4)
+World.add(world, ball);
+
+document.addEventListener('keydown', (event) => {
+    const { x, y } = ball.velocity;
+
+    if (event.keyCode === 38) {
+        Body.setVelocity(ball, { x, y: y - 5 });
+    }
+
+    if (event.keyCode === 39) {
+        Body.setVelocity(ball, { x: x + 5, y });
+    }
+
+    if (event.keyCode === 40) {
+        Body.setVelocity(ball, { x, y: y + 5 });
+    }
+
+    if (event.keyCode === 37) {
+        Body.setVelocity(ball, { x: x - 5, y });
+    }
+});
